9d57c941da7b428a320b58a598358414fd977860
Mikael Mayer <mimayere@amazon.com>
2024-07-03 15:21:10 +0200

    Removed externs

--- DynamoDbEncryption/runtimes/rust/build.sh
+++ DynamoDbEncryption/runtimes/rust/build.sh
@@ -8,7 +8,6 @@ fi
 MPL=runtimes/rust/dafny_impl/src
 IMPL=runtimes/rust/dafny_impl/src
 ALL_EXTERNS=$(ls dafny_impl/src | grep .rs | grep -v ImplementationFromDafny.rs | xargs -I{} echo $IMPL/{})
-REMOVE_EXTERNS=$(ls ImplementationFromDafny-rust/src | grep .rs | grep -v ImplementationFromDafny.rs | xargs -I{} echo ImplementationFromDafny-rust/src/{})
 
 (cd ../../../DynamoDbEncryption;
 $DAFNY  translate rs --no-verify --emit-uncompilable-code:true \
@@ -17,5 +16,6 @@ $DAFNY  translate rs --no-verify --emit-uncompilable-code:true \
 --include-runtime:true --output runtimes/rust/ImplementationFromDafny UniqueToBuildInRust.dfy $ALL_EXTERNS)
 
 echo "Removing externs";
+REMOVE_EXTERNS=$(ls ImplementationFromDafny-rust/src | grep .rs | grep -v ImplementationFromDafny.rs | xargs -I{} echo ImplementationFromDafny-rust/src/{})
 # Now remove all *.rs files 
 rm $REMOVE_EXTERNS
