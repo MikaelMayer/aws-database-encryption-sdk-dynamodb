7e10c51099cb182ec5a2253299a13c01031c2f64
Andy Jewell <ajewell@amazon.com>
2024-07-08 16:22:46 -0400

    digest

--- submodules/runtimes/rust/dafny_impl/src/digest.rs
+++ submodules/runtimes/rust/dafny_impl/src/digest.rs
@@ -6,12 +6,14 @@
 
 pub mod ExternDigest {
     use crate::*;
+    use aws_lc_rs::digest;
+
     pub struct _default {}
     impl _default {
         #[allow(non_snake_case)]
         pub fn Digest(
-            _digestAlgorithm: &::std::rc::Rc<r#_software_damazon_dcryptography_dprimitives_dinternaldafny_dtypes::DigestAlgorithm>,
-            _message: &::dafny_runtime::Sequence<u8>,
+            digestAlgorithm: &::std::rc::Rc<r#_software_damazon_dcryptography_dprimitives_dinternaldafny_dtypes::DigestAlgorithm>,
+            message: &::dafny_runtime::Sequence<u8>,
         ) -> ::std::rc::Rc<
             Wrappers::Result<
                 ::dafny_runtime::Sequence<u8>,
@@ -20,7 +22,16 @@ pub mod ExternDigest {
                 >,
             >,
         > {
-            todo!("Digest not implemented");
+            let algorithm = match digestAlgorithm {
+                SHA_512 => &digest::SHA512,
+                SHA_384 => &digest::SHA384,
+                SHA_256 => &digest::SHA256,
+            };
+            let message_vec: Vec<u8> = message.iter().collect();
+            let result = digest::digest(&algorithm, &message_vec);
+            ::std::rc::Rc::new(Wrappers::Result::Success {
+                value: result.as_ref().iter().cloned().collect(),
+            })
         }
     }
 }
