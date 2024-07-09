3f56d2d4da973de830005e4537368acf462b5883
Andy Jewell <ajewell@amazon.com>
2024-07-09 11:41:50 -0400

    MutableMap

--- submodules/runtimes/rust/dafny_impl/src/dafny_libraries.rs
+++ submodules/runtimes/rust/dafny_impl/src/dafny_libraries.rs
@@ -3,21 +3,21 @@
 
 #![allow(warnings, unconditional_panic)]
 #![allow(nonstandard_style)]
+
 pub mod DafnyLibraries {
+    use dashmap::DashMap;
+    use std::collections::HashMap;
+    use std::collections::HashSet;
+
     pub struct MutableMap<K: ::dafny_runtime::DafnyTypeEq, V: ::dafny_runtime::DafnyTypeEq> {
-        _phantom_type_param_0: ::std::marker::PhantomData<K>,
-        _phantom_type_param_1: ::std::marker::PhantomData<V>,
+        map: DashMap<K, V>,
     }
 
     impl<K: ::dafny_runtime::DafnyTypeEq, V: ::dafny_runtime::DafnyTypeEq> MutableMap<K, V> {
         pub fn _allocate_object() -> ::dafny_runtime::Object<Self> {
-            // SAFETY: The Rc has not been shared before
-            unsafe {
-                ::dafny_runtime::Object::from_rc(::std::rc::Rc::new(MutableMap {
-                    _phantom_type_param_0: todo!(),
-                    _phantom_type_param_1: todo!(),
-                }))
-            }
+            ::dafny_runtime::Object::new(MutableMap {
+                map: DashMap::new(),
+            })
         }
     }
 
@@ -25,31 +25,47 @@ pub mod DafnyLibraries {
         crate::DafnyLibraries::MutableMapTrait<K, V> for MutableMap<K, V>
     {
         fn content(&self) -> ::dafny_runtime::Map<K, V> {
-            todo!("MutableMap::content not implemented");
+            let mut new_map = HashMap::new();
+            for entry in self.map.iter() {
+                new_map.insert(entry.key().clone(), entry.value().clone());
+            }
+            dafny_runtime::Map::from_hashmap_owned(new_map)
         }
-        fn Put(&mut self, _k: &K, _v: &V) -> () {
-            todo!("MutableMap::Put not implemented");
+        fn Put(&mut self, k: &K, v: &V) -> () {
+            self.map.insert(k.clone(), v.clone());
         }
         fn Keys(&self) -> ::dafny_runtime::Set<K> {
-            todo!("MutableMap::Keys not implemented");
+            let mut new_set = HashSet::new();
+            for entry in self.map.iter() {
+                new_set.insert(entry.key().clone());
+            }
+            dafny_runtime::Set::from_hashset_owned(new_set)
         }
-        fn HasKey(&self, _k: &K) -> bool {
-            todo!("MutableMap::HasKey not implemented");
+        fn HasKey(&self, k: &K) -> bool {
+            self.map.contains_key(k)
         }
         fn Values(&self) -> ::dafny_runtime::Set<V> {
-            todo!("MutableMap::Values not implemented");
+            let mut new_set = HashSet::new();
+            for entry in self.map.iter() {
+                new_set.insert(entry.value().clone());
+            }
+            dafny_runtime::Set::from_hashset_owned(new_set)
         }
         fn Items(&self) -> ::dafny_runtime::Set<(K, V)> {
-            todo!("MutableMap::Items not implemented");
+            let mut new_set = HashSet::new();
+            for entry in self.map.iter() {
+                new_set.insert((entry.key().clone(), entry.value().clone()));
+            }
+            dafny_runtime::Set::from_hashset_owned(new_set)
         }
-        fn Select(&self, _k: &K) -> V {
-            todo!("MutableMap::Select not implemented");
+        fn Select(&self, k: &K) -> V {
+            self.map.get(k).unwrap().clone()
         }
-        fn Remove(&mut self, _k: &K) -> () {
-            todo!("MutableMap::Remove not implemented");
+        fn Remove(&mut self, k: &K) -> () {
+            self.map.remove(k);
         }
         fn Size(&self) -> ::dafny_runtime::DafnyInt {
-            todo!("MutableMap::Size not implemented");
+            self.map.len().into()
         }
     }
 
