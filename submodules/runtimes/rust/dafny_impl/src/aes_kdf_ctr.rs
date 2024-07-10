09f10194f48d78920cb3585a007f9790f0e6221f
Andy Jewell <ajewell@amazon.com>
2024-07-10 15:35:59 -0400

    m

--- submodules/runtimes/rust/dafny_impl/src/aes_kdf_ctr.rs
+++ submodules/runtimes/rust/dafny_impl/src/aes_kdf_ctr.rs
@@ -3,14 +3,56 @@
 
 #![allow(warnings, unconditional_panic)]
 #![allow(nonstandard_style)]
-use crate::*;
 
 pub mod AesKdfCtr {
+    use crate::software::amazon::cryptography::primitives::internaldafny::types::Error as DafnyError;
+    use crate::*;
+    use ::dafny_runtime::Sequence;
+    use aws_lc_rs::cipher::{DecryptingKey, EncryptingKey, UnboundCipherKey, AES_256};
+    use aws_lc_rs::error::Unspecified;
+    use std::rc::Rc;
+
     pub struct _default {}
 
+    fn error(s: &str) -> Rc<DafnyError> {
+        Rc::new(DafnyError::AwsCryptographicPrimitivesError {
+            message:
+                dafny_runtime::dafny_runtime_conversions::unicode_chars_false::string_to_dafny_string(s),
+        })
+    }
+
+    fn ctr_stream(nonce: &[u8], key: &[u8], length: u32) -> Result<Vec<u8>, Unspecified> {
+        let mut in_out_buffer = Vec::new();
+        in_out_buffer.resize(length as usize, 0);
+
+        let key = UnboundCipherKey::new(&AES_256, key)?;
+        let mut encrypting_key = EncryptingKey::ctr(key)?;
+        let context = encrypting_key.encrypt(&mut in_out_buffer)?;
+        Ok(in_out_buffer)
+    }
+
     impl _default {
-        pub fn AesKdfCtrStream(nonce: &::dafny_runtime::Sequence<u8>, key: &::dafny_runtime::Sequence<u8>, length: u32) -> ::std::rc::Rc<super::Wrappers::Result<::dafny_runtime::Sequence<u8>, crate::software::amazon::cryptography::primitives::internaldafny::types::OpaqueError>>{
-            todo!("AesKdfCtrStream not implemented")
+        pub fn AesKdfCtrStream(
+            nonce: &Sequence<u8>,
+            key: &Sequence<u8>,
+            length: u32,
+        ) -> Rc<
+            Wrappers::Result<
+                Sequence<u8>,
+                Rc<crate::software::amazon::cryptography::primitives::internaldafny::types::Error>,
+            >,
+        > {
+            let nonce: Vec<u8> = nonce.iter().collect();
+            let key: Vec<u8> = key.iter().collect();
+            match ctr_stream(&nonce, &key, length) {
+                Ok(x) => Rc::new(Wrappers::Result::Success {
+                    value: x.iter().cloned().collect(),
+                }),
+                Err(e) => {
+                    let msg = format!("{}", e);
+                    Rc::new(Wrappers::Result::Failure { error: error(&msg) })
+                }
+            }
         }
     }
 }
