fe8e87237c0ed29ad667463d5f3b5a3e1002eb17
Andy Jewell <ajewell@amazon.com>
2024-08-16 11:10:42 -0400

    m

---
+++ DynamoDbEncryption/runtimes/rust/dafny_impl/import_kms.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+cd $( dirname -- "${BASH_SOURCE[0]}" )
+
+rm -rf src/kms
+cp -r /Users/ajewell/gzg/smithy-dafny/TestModels/aws-sdks/kms-lite/runtimes/rust/src src/kms
+rm -f src/kms/implementation_from_dafny.rs
+find src/kms -name '*.rs' | fgrep -v ImplementationFromDafny | xargs perl -i -pe 's/crate::_Wrappers_Compile/crate::Wrappers/'
+find src/kms -name '*.rs' | fgrep -v ImplementationFromDafny | xargs perl -i -pe 's/crate::r#_Wrappers_Compile/crate::Wrappers/'
+find src/kms -name '*.rs' | fgrep -v ImplementationFromDafny | xargs perl -i -pe 's/mpl_standard_library::_Wrappers_Compile/crate::Wrappers/'
+find src/kms -name '*.rs' | fgrep -v ImplementationFromDafny | xargs perl -i -pe 's/crate::conversions/crate::kms::conversions/'
+find src/kms -name '*.rs' | fgrep -v ImplementationFromDafny | xargs perl -i -pe 's/crate::standard_library_conversions/crate::kms::standard_library_conversions/'
+cat extra_kms.txt >> src/kms/client.rs
